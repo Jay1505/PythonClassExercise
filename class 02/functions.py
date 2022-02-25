27a23eb0bfbddec09fc36335374c8f84a66ed73d
Jay Ankurkumar Shah <jayshah1505@gmail.com>
2022-02-24 19:59:01 -0500

    Completed class 02

--- class 02/functions.py
+++ class 02/functions.py
@@ -25,3 +25,22 @@ def greet_by_name(name='joke'):#here if you dont input any value of name while c
     print(f'Hello {name}')
 
 greet_by_name('Jay')
+
+#In class exercises
+#Q.1 Create a function that takes and returns all the even numbers from 0 to the given number
+def even_numbers(number):
+    EvenNumberList=[]
+    for i in range(number):
+        if i%2==0:
+            EvenNumberList.append(i)
+    return EvenNumberList
+
+
+
+#Q.2 Call the function and store the result
+a=even_numbers(7)
+
+#Q.3 Print the list and in reverse
+print(a)
+#reverse
+print(a[::-1])
